f3cc0c36da76d55b768f57de2990ae41de8ae1ee
UKX0502 <deepa.balasubramanian@infotel.com>
2025-09-01 13:30:15 +0530

    ngTemplateOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Flag to control login state
-  isLoggedIn = false;
+  // Example data
+  user = {
+    name: 'Deepa Balasubramanian',
+    role: 'Frontend Developer',
+    description: 'Passionate about Angular and UI/UX design.'
+  };
 
-  // Method to toggle login/logout
-  toggleLogin() {
-    this.isLoggedIn = !this.isLoggedIn;
+  // Toggle between templates
+  showDetailed = true;
+
+  toggleView() {
+    this.showDetailed = !this.showDetailed;
   }
 }
