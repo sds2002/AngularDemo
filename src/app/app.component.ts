5458bcbdf3f757505b4515920bd5021aa6de24a0
UKX0502 <deepa.balasubramanian@infotel.com>
2025-09-01 13:32:44 +0530

    Special Variables in *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Example data
-  user = {
-    name: 'Deepa Balasubramanian',
-    role: 'Frontend Developer',
-    description: 'Passionate about Angular and UI/UX design.'
-  };
-
-  // Toggle between templates
-  showDetailed = true;
-
-  toggleView() {
-    this.showDetailed = !this.showDetailed;
-  }
+  items = ['Angular', 'React', 'Vue', 'Svelte'];
 }
