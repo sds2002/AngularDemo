245f32ff1085ea1a1b5e9272d111765b72e5911e
UKX0502 <deepa.balasubramanian@infotel.com>
2025-09-01 13:35:20 +0530

    emplate Input Variables (let-)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items = ['Angular', 'React', 'Vue', 'Svelte'];
+  languages = ['Angular', 'React', 'Vue', 'Svelte'];
 }
